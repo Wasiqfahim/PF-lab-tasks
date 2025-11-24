2270046a7dd158540529a123006e919e282e0ae0
Wasiqfahim <f2025-0245@bnu.edu.pk>
2025-11-24 22:02:45 +0500

    Add files via upload

---
+++ pf lab2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"enter value of a";
+	cin>>a;
+	cout<<"enter value of b";
+	cin>>b;
+	cout<<"the sum is:"<<a+b<<endl;
+	cout<<"the difference is:"<<a-b<<endl;
+	cout<<"the product is"<<a*b<<endl;
+	cout<<"the quotient is:"<<a/b<<endl;
+	return 0;
+}
