92111eb3bec85e83bd218289004d2eb7e6d6a003
Wasiqfahim <f2025-0245@bnu.edu.pk>
2025-11-24 22:46:05 +0500

    Add files via upload

---
+++ LAB 2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float farenheit;
+	float temp;
+	cout<<"temp in celsius"<<endl;
+	cin>>temp;
+	farenheit=(temp*9/5)+32;
+	cout<<"temp in farenheit is"<<farenheit<<endl;
+	return 0;
+}
